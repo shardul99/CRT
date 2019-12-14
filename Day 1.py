56ca18702746c39117f9675d5778e2274973ddb9
shardul99 <40022332+shardul99@users.noreply.github.com>
2019-12-14 09:59:08 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Jupyter Notebook
+# ## Jupyter Notebook
+# ### Jupyter Notebook
+# #### Jupyter Notebook
+# ##### Jupyter Notebook
+# ###### Jupyter Notebook
+# 
+
+# no hash used here
+# 
+
+# - these are bullet points 
+
+# - point 
+#     - **subpoint** 
+#     - *this is italic*
+#     - *** this is a bold statement***
+#     
+
+# In[1]:
+
+
+print("this is a print statement")
+
+
+# In[2]:
+
+
+print("Markdown text",end = ' ')
+print("Code cell")
+
+
+# In[ ]:
+
+
+a = input(" enter a string")
+print ("a" in a)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
